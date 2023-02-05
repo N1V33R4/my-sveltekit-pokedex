274c21b14403761d5358ddbb10e8f942fffdee04
N1V33R4 <75680359+N1V33R4@users.noreply.github.com>
2023-02-05 16:27:01 +0700

    Change about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,10 @@
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
 <p class="text-lg">This is a really cool Svelte Kit demo that uses the Pokemon API to build a Pokedex.</p>
 
+<br>
+
 <p class="text-sm">Hosted by 
-  <a class="underline text-red-500" href="#">Hou Neavireakpong</a> on 
-  <a class="" href="#">Render</a>
+  <a class="underline text-blue-500" href="https://github.com/N1V33R4">Hou Neavireakpong</a> on 
+  <a class="underline text-blue-500" href="https://render.com">Render</a>.
 </p>
+<p>Link to <a class="underline text-blue-500" href="https://www.youtube.com/watch?v=UU7MgYIbtAk">original video</a></p>
